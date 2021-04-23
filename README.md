da73823b234daf3622866a2201892b46024d0495
Djonatas Borges <interdjonatas@gmail.com>
2021-04-23 14:07:52 -0300

    Criada Api de playlist de músicas.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # playlist-web-api
 Trata de uma API de Playlist de músicas.
+Ela Adiciona, deleta e mostra as músicas existentes na playlist.
+Ela também edita músicas já adicionadas e procura musicas por índice.
---
+++ playlist.py
@@ -0,0 +1,75 @@
+from flask import Flask, jsonify, request
+import requests
+
+playlist = Flask(__name__)
+
+musicas = [
+    {
+        "musica": "In the End",
+        "estilo": "New Metal",
+        "id": 0
+    },
+    {
+        "musica": "Eu bebo se beija",
+        "estilo": "Sertanejo Universitário",
+        "id": 1
+    },
+    {
+        "musica": "Menino da Porteira",
+        "estilo": "Sertanejo Raiz",
+        "id": 2
+    },
+    {
+        "musica": "Dependente",
+        "estilo": "Pagode",
+        "id": 3
+    },
+    {
+        "musica": "Jesus Chorou",
+        "estilo": "Rap",
+        "id": 4
+    }
+]
+
+
+@playlist.route('/')
+def obter_musicas():
+    return jsonify(musicas)
+
+
+@playlist.route('/<int:indice>', methods=['GET'])
+def obter_musicas_por_id(indice):
+    try:
+        if musicas[indice] is not None:
+            return jsonify(musicas[indice])
+    except:
+        return jsonify(f'A música de núnero {indice} não esta disponível. A playlist está com {len(musicas)} músicas até o momento. Lembramos que a música começa no índice 0', 404)
+
+
+@playlist.route('/musicas', methods=['POST'])
+def postar_musica():
+    musica = request.get_json()
+    musicas.append(musica)
+    return jsonify(musica, 200)
+
+
+@playlist.route('/musicas/<int:indice>', methods=['PUT'])
+def repost(indice):
+    repostagem = request.get_json()
+    musicas[indice].update(repostagem)
+
+    return jsonify(musicas[indice], 200)
+
+
+@playlist.route('/musicas/<int:indice>', methods=['DELETE'])
+def deletar(indice):
+    try:
+        if musicas[indice] is not None:
+            deletada = musicas[indice]['musica']
+            del musicas[indice]
+            return jsonify(f'A música {deletada} foi deletada', 200)
+    except:
+        return jsonify('Não foi possível encontrar a música para exclusão', 404)
+
+
+playlist.run(port=9999, host='localhost', debug=True)
